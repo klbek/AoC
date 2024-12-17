0a6c74360cbe3ab9b63931ffd401985ce3e294a2
KlaraBek <sadecka.kl@gmail.com>
2024-12-17 23:54:30 +0100

    day1 part2

--- 2024/day01.py
+++ 2024/day01.py
@@ -5,17 +5,23 @@ with open(r'2024\input01.txt', encoding='utf-8') as input_file:
     file = input_file.readlines()
     for line in file:
         sep_line = line.strip().split('   ')
-        left_list.append(sep_line[0])
-        right_list.append(sep_line[1])
+        left_list.append(int(sep_line[0]))
+        right_list.append(int(sep_line[1]))
 
 left_list.sort()
 right_list.sort()
 
 distance_list = list()
-for i in range(len(left_list)):
-    distance = abs(int(left_list[i]) - int(right_list[i]))
-    distance_list.append(distance)
-   
+similarity_list = list()
 
-sum_distance = sum(distance_list)
-print(sum_distance)
+for i in range(len(left_list)):
+    distance = abs(left_list[i] - right_list[i])
+    distance_list.append(distance)
+
+    count = right_list.count(left_list[i])
+    result = left_list[i] * count
+    similarity_list.append(result)   
+
+print(f'Distance: {sum(distance_list)}')
+print(f'Similarity score: {sum(similarity_list)}')
+
